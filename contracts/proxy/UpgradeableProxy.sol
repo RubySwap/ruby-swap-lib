741639cdc707c2158ccc29c5b177af44c1bad9b3
VecDeque <tom.wilson87@abine.us>
2021-05-18 03:47:12 -0500

    Move to Byzantium

--- contracts/proxy/UpgradeableProxy.sol
+++ contracts/proxy/UpgradeableProxy.sol
@@ -68,8 +68,6 @@ contract UpgradeableProxy is Proxy {
      * @dev Stores a new address in the EIP1967 implementation slot.
      */
     function _setImplementation(address newImplementation) private {
-        require(Address.isContract(newImplementation), "UpgradeableProxy: new implementation is not a contract");
-
         bytes32 slot = _IMPLEMENTATION_SLOT;
 
         // solhint-disable-next-line no-inline-assembly
